c3e96f7f63a0d3306290699e99d7a5b416d06cc2
Athena Anil <152438547+athee010@users.noreply.github.com>
2025-05-26 11:18:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+# PostgreSQL Database Config
+DB_HOST = "your-db-host"
+DB_NAME = "your-db-name"
+DB_USER = "your-db-user"
+DB_PASSWORD = "your-db-password"
+
+# AWS S3 Config
+BUCKET = "your-bucket"
+PREFIX = "your/prefix/"  # e.g., "uploads/"
+LIMIT = 10  # Max number of files to process per run
